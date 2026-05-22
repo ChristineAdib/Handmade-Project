874148a6ba7652dd54822f9ff4387aceff5ac91c
Steven Ayman <stevenaymansalah@gmail.com>
2026-05-23 02:02:14 +0300

    fix: comment out Images property in CreateProductDto and ignore build files

--- Handora/HandoraApplication/DTOs/ProductDTOs/CreateProductDto.cs
+++ Handora/HandoraApplication/DTOs/ProductDTOs/CreateProductDto.cs
@@ -8,5 +8,15 @@ namespace HandoraApplication.DTOs.ProductDTOs
 {
     internal class CreateProductDto
     {
+        public string TitleEn { get; set; }
+        public string TitleAr { get; set; }
+        public string? DescriptionEn { get; set; }
+        public string? DescriptionAr { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid ShopId { get; set; }
+        //public List<IFormFile> Images { get; set; } // Multiple images
+        public List<string> Tags { get; set; }
     }
 }
