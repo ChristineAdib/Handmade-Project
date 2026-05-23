e14a0cb511c1d3dfaa8938a7681bd56e2d7998bd
ChristineAdib <christineadib8@gmail.com>
2026-05-23 15:57:20 +0300

    add login & register

---
+++ Handora/HandoraApplication/DTOs/AuthDTOs/AuthResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandoraApplication.DTOs.AuthDTOs
+{
+    public sealed record AuthResponseDto
+    {
+        public string UserId { get; init; } = string.Empty;
+        public string Name { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
+        public string Token { get; init; } = string.Empty;
+        public DateTime TokenExpiry { get; init; }
+        public IList<string> Roles { get; init; } = [];
+    }
+}
---
+++ Handora/HandoraApplication/IServices/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using HandoraApplication.DTOs.AuthDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandoraApplication.IServices
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterDto dto, CancellationToken ct = default);
+        Task<AuthResponseDto> LoginAsync(LoginDto dto, CancellationToken ct = default);
+    }
+}
