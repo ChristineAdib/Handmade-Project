eba33887c8037e7a3835e160a1b38f084ea0e34f
sahar <sahar@LAPTOP-JR0CTTM9>
2026-05-23 20:15:01 +0300

    shope services and controliier

---
+++ Handora/HandoraApplication/IServices/IShopService.cs
@@ -0,0 +1,17 @@
+﻿using HandoraApplication.DTOs.ShopDTOs;
+using HandoraApplication.Helpers;
+
+namespace HandoraApplication.IServices;
+
+public interface IShopService
+{
+    Task<Result<ShopDto>> GetShopById(Guid id);
+    Task<Result<ShopDto>> GetShopWithProducts(Guid id);
+    Task<Result<ShopDto>> GetMyShop(string ownerId);
+    Task<Result<ShopStatsDto>> GetShopStats(Guid id);
+    Task<Result<IEnumerable<ShopDto>>> GetTopRatedShops(int count = 10);
+    Task<Result<IEnumerable<ShopDto>>> SearchShops(ShopFilterDto filter);
+    Task<Result<ShopDto>> CreateShop(string ownerId, CreateShopDto dto);
+    Task<Result<ShopDto>> UpdateShop(Guid id, string ownerId, UpdateShopDto dto);
+    Task<Result> DeleteShop(Guid id, string ownerId);
+}
