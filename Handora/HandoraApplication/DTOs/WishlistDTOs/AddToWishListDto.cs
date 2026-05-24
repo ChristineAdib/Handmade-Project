dde7bf9b14d9e163327bfb833e45f148b7ca2266
Manar Abdel-Hak <manarabdelhak186@gmail.com>
2026-05-24 14:51:59 +0200

    add wishlist feature

---
+++ Handora/HandoraApplication/DTOs/WishlistDTOs/AddToWishListDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandoraApplication.DTOs.WishlistDTOs
+{
+   public class AddToWishListDto
+    {
+        public Guid ProductId { get; set; }
+    }
+}
---
+++ Handora/HandoraApplication/IServices/IWishListService.cs
@@ -0,0 +1,17 @@
+﻿using HandoraApplication.DTOs.WishlistDTOs;
+using HandoraApplication.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandoraApplication.IServices
+{
+   public interface IWishListService
+    {
+        Task<Result<WishListDto>> GetWishListAsync(string userId);
+        Task<Result<WishListDto>> AddItemAsync(string userId, AddToWishListDto dto);
+        Task<Result<WishListDto>> RemoveItemAsync(string userId, Guid productId);
+    }
+}
