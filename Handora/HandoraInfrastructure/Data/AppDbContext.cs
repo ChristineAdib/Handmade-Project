27423f0d5b430e74cfef93d5f9254b7db0f8e136
Amira <am4126199@gmail.com>
2026-05-25 14:36:35 +0300

    feat: add tag, review, category services and controllers

--- Handora/HandoraInfrastructure/Data/AppDbContext.cs
+++ Handora/HandoraInfrastructure/Data/AppDbContext.cs
@@ -39,7 +39,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options):IdentityDbCont
     public DbSet<WithdrawalRequest> WithdrawalRequests { get; set; }
     public DbSet<HandoraDomain.Models.AppUser.Address> Addresses { get; set; }
     public DbSet<OtpVerification> OtpVerifications { get; set; }
-    public DbSet<Address> Addresses { get; set; }
+    //public DbSet<Address> Addresses { get; set; }
     public DbSet<Follow> Follows { get; set; }
 
 
