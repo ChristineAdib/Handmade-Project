e6384b892b99e9aa9a97c39036312c6264f2b989
ChristineAdib <christineadib8@gmail.com>
2026-06-01 12:33:01 +0300

    update migrations and dtabase

--- Handora/HandoraInfrastructure/Data/AppDbContext.cs
+++ Handora/HandoraInfrastructure/Data/AppDbContext.cs
@@ -33,25 +33,21 @@ public class AppDbContext(DbContextOptions<AppDbContext> options):IdentityDbCont
     public DbSet<Review> Reviews { get; set; }
     public DbSet<Notification> Notifications { get; set; }
     public DbSet<Coupon> Coupons { get; set; }
-<<<<<<< HEAD
 
-=======
     public DbSet<HandoraDomain.Models.AppUser.Address> Addresses { get; set; }
     public DbSet<OtpVerification> OtpVerifications { get; set; }
->>>>>>> main
     public DbSet<OrderShippingAddress> OrderShippingAddresses { get; set; }
 
     public DbSet<SellerBalanceTransaction> SellerBalanceTransactions { get; set; }
 
     public DbSet<WithdrawalRequest> WithdrawalRequests { get; set; }
-<<<<<<< HEAD
-    public DbSet<HandoraDomain.Models.AppUser.Address> Addresses { get; set; }
-    public DbSet<OtpVerification> OtpVerifications { get; set; }
+
+    //public DbSet<HandoraDomain.Models.AppUser.Address> Addresses { get; set; }
+    //public DbSet<OtpVerification> OtpVerifications { get; set; }
 
     //public DbSet<Address> Addresses { get; set; }
     //public DbSet<Address> Addresses { get; set; }
-=======
->>>>>>> main
+
     public DbSet<Follow> Follows { get; set; }
 
     //public DbSet<Address> Addresses { get; set; }
