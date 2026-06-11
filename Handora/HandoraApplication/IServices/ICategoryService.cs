7e7f9106a9dad77b1dd22a0d649ad5c7db98dffe
sahar <sahar@LAPTOP-JR0CTTM9>
2026-06-11 22:38:24 +0300

    اكتب رسالة توضح تعديلاتك هنا

--- Handora/HandoraApplication/IServices/ICategoryService.cs
+++ Handora/HandoraApplication/IServices/ICategoryService.cs
@@ -8,4 +8,7 @@ public interface ICategoryService
     Task<Result<IEnumerable<CategoryResponseDto>>> GetAllCategories();
     Task<Result<CategoryResponseDto>> GetCategoryById(Guid id);
     Task<Result<IEnumerable<CategorySummaryDto>>> GetSubCategories(Guid parentId);
+    Task<Result<CategoryResponseDto>> CreateCategory(CreateCategoryDto dto);
+    Task<Result<CategoryResponseDto>> UpdateCategory(Guid id, UpdateCategoryDto dto);
+    Task<Result<bool>> DeleteCategory(Guid id);
 }
