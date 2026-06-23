5e47f45af040e39d959e1e31f237d7707dc10136
Manar568 <manarabdelhak186@gmail.com>
2026-06-24 01:44:19 +0300

    implement AI product agent for image analysis

---
+++ Handora/HandoraApplication/IServices/IProductAgentService.cs
@@ -0,0 +1,14 @@
+﻿using HandoraApplication.DTOs.ProductAgentDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandoraApplication.IServices
+{
+    public interface IProductAgentService
+    {
+        Task<ProductAnalysisResult> AnalyzeProductImageAsync(string imageBase64, string mimeType);
+    }
+}
